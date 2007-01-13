a3695860894c977425079dc31456f316470aa8e9
Danilo Šegan <danilo@src.gnome.org>
2007-01-13 11:03:15 +0000

    Optimize to load MO file only once.
    svn path=/trunk/; revision=186

--- l10n.py
+++ l10n.py
@@ -7,6 +7,19 @@ import defaults
 import accept
 import os
 
+trans = None
+
+def get_trans():
+    global trans
+    if trans: return trans
+    filename = 'po/%s.mo' % (defaults.language)
+    try:
+        fp = open(filename, 'rb')
+        trans = GNUTranslations(fp=fp)
+    except:
+        trans = NullTranslations()
+    return trans
+
 def set_language():
     fallback = defaults.language
     linguas = open('po/LINGUAS', 'r')
@@ -14,25 +27,14 @@ def set_language():
 
     select = accept.language(os.getenv('HTTP_ACCEPT_LANGUAGE', fallback))
     defaults.language = select.select_from(langs)
+    get_trans()
 
 def gettext(text):
-    filename = 'po/%s.mo' % (defaults.language)
-    try:
-        fp = open(filename, 'rb')
-        trans = GNUTranslations(fp=fp)
-    except:
-        trans = NullTranslations()
-    return trans.ugettext(unicode(text))
+    return get_trans().ugettext(unicode(text))
 
 
 def ngettext(text, plural, number):
-    filename = 'po/%s.mo' % (defaults.language)
-    try:
-        fp = open(filename, 'rb')
-        trans = GNUTranslations(fp=fp)
-    except:
-        trans = NullTranslations()
-    return trans.ungettext(unicode(text), unicode(plural), number)
+    return get_trans().ungettext(unicode(text), unicode(plural), number)
 
 class MyFilter(Cheetah.Filters.Filter):
     def filter(self, val, **kw):
