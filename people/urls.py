0469edb78c7393ab6ab3fe04c257509fb68fe69e
Stéphane Raimbault <stephane.raimbault@gmail.com>
2008-11-27 21:33:55 +0000

    Fixed people list.
    2008-11-27  Stéphane Raimbault  <stephane.raimbault@gmail.com>
    
    	* people/urls.py: Fixed people list.
    
    svn path=/trunk/; revision=1193

--- people/urls.py
+++ people/urls.py
@@ -1,18 +1,23 @@
 from django.conf.urls.defaults import *
 from people.models import Person
 
-info_dict = {
+
+info_dict_list = {
     'queryset': Person.objects.all(),
-    'slug_field': 'username',
     'template_object_name': 'person',
     'extra_context': { 
         'pageSection': "teams"
     }
 }
 
+info_dict_detail = dict(
+    info_dict_list,
+    slug_field = 'username'
+)
+    
 urlpatterns = patterns('django.views.generic.list_detail',
-    url(r'^$', 'object_list', dict(info_dict), 'persons'),                    
-    url(r'(?P<object_id>\d+)/$', 'object_detail', dict(info_dict), 'person'),
+    url(r'^$', 'object_list', dict(info_dict_list), 'persons'),                    
+    url(r'(?P<object_id>\d+)/$', 'object_detail', dict(info_dict_detail), 'person'),
     # equivalent to the previous, but using username instead of user pk
-    url(r'(?P<slug>\w+)/$', 'object_detail', dict(info_dict), 'person'),
+    url(r'(?P<slug>\w+)/$', 'object_detail', dict(info_dict_detail), 'person'),
 )
